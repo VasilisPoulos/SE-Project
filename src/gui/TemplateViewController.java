c50ea2f0a90910b45874c28e19479ab527b02e72
Christos Zonios <czonios@gmail.com>
2018-04-10 03:56:37 +0300

    ADD a lot of styling to buttons etc.

--- src/gui/TemplateViewController.java
+++ src/gui/TemplateViewController.java
@@ -3,9 +3,13 @@ package gui;
 import datamodel.PatternLanguage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -23,9 +27,7 @@ public class TemplateViewController {
     @FXML
     public void populateTemplates() {
 
-        /* TODO: Make buttonList into a list of Hboxes, so that they are center-aligned
-         * TODO: Style buttons, add paddings etc. */
-        List<Button> buttonList = new ArrayList<>(); //our Collection to hold newly created Buttons
+        List<HBox> buttonList = new ArrayList<>(); //our Collection to hold created Button objects (HBox for styling)
 
 //            ArrayList<Template> templatesList = getTemplatesList();
 
@@ -38,13 +40,45 @@ public class TemplateViewController {
              * using its index as id and set the event handler
              */
             for (int i = 0; i < templatesList.size(); i++) { //iterate over every row returned
-                String title = templatesList.get(i).toString(); // get title of template i
-                Button btn = new Button(title);
-                btn.setId(Integer.toString(i));
-                btn.setOnAction((e) -> this.handlePickPattern(e));
-                buttonList.add(btn);
+
+                String title = templatesList.get(i).toString();     // get title of template i
+                HBox hbox = new HBox();                             // Create the HBox container for the button
+                Button btn = new Button(title);                     // Create the Button
+                btn.setId(Integer.toString(i));                     // Set button id to its index
+                btn.setOnAction((e) -> this.handlePickPattern(e));  // Set button handler to handlePickPattern
+
+                /* Create left and right regions to center-align the button
+                 * and set their attributes so that they are dynamically set
+                 */
+                Region regionLeft = new Region();
+                Region regionRight = new Region();
+
+                HBox.setHgrow(hbox, Priority.ALWAYS);
+                HBox.setHgrow(regionLeft, Priority.ALWAYS);
+                HBox.setHgrow(regionRight, Priority.ALWAYS);
+
+                regionLeft.setMinSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+                regionRight.setMinSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+                regionLeft.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+                regionRight.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+
+                regionLeft.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+                regionRight.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+                hbox.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+
+                btn.setPadding(new Insets(10));
+
+                /* Add the button and regions to the HBox */
+                hbox.getChildren().add(regionLeft);
+                hbox.getChildren().add(btn);
+                hbox.getChildren().add(regionRight);
+
+                /* Finally, add the hbox containing the button to the list */
+                buttonList.add(hbox);
             }
 
+            /* Add buttons to gui */
+            templateContainer.setSpacing(20);
             templateContainer.getChildren().clear(); //remove all Buttons that are currently in the container
             templateContainer.getChildren().addAll(buttonList); // add new Buttons from the list
 
