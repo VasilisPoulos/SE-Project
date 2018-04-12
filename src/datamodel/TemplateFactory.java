812bd38b3cbf7ee6f2778ca3c4dffa2431361505
Vasilis Poulos <cse42805@cs.uoi.gr>
2018-04-12 18:22:43 +0300

    ADD template factory changes - test

--- src/datamodel/TemplateFactory.java
+++ src/datamodel/TemplateFactory.java
@@ -3,14 +3,26 @@ package datamodel;
 public class TemplateFactory
 {
     private Pattern[] templateList;
+    private static int template_counter = 0;
 
-    public TemplateFactory()
+    TemplateFactory()
     {
         templateList = new Pattern[3];
     }
 
     public Pattern createTemplate(String templateName)
     {
-        return null;
+        return new Pattern(templateName);
+    }
+
+    public Pattern[] templateListIterator()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            templateList[i] = createTemplate("default"+i);
+            template_counter++;
+        }
+
+        return templateList;
     }
 }
---
+++ src/datamodel/TemplateFactoryTest.java
@@ -0,0 +1,19 @@
+package datamodel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemplateFactoryTest
+{
+    @org.junit.jupiter.api.Test
+    void shouldMakeTemplates()
+    {
+        String[] ex_result = {"default0", "default1", "default2"};
+        TemplateFactory factory = new TemplateFactory();
+        Pattern[] result = factory.templateListIterator();
+        for (int i = 0; i < 3; i++)
+        {
+            assertEquals(ex_result[i], result[i].name);
+        }
+    }
+
+}
