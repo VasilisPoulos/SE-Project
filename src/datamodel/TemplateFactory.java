77bd571822ff10489560f2d5461b06e1ef1df176
SofiaPasoi <sofaki1926@hotmail.gr>
2018-04-14 22:43:09 +0300

    templates

--- src/datamodel/TemplateFactory.java
+++ src/datamodel/TemplateFactory.java
@@ -21,10 +21,18 @@ public class TemplateFactory
         micro.add(new PatternPart("Name", "What shall this pattern be called by practitioners?"));
         micro.add(new PatternPart("Template", "What shall this pattern be called by practitioners?"));
         micro.add(new PatternPart("Problem", "What shall this pattern be called by practitioners?"));
-        micro.add(new PatternPart("Solution", "What shall this pattern be called by practitioners?"));
+
+        Pattern inductive = new Pattern( "InductiveMiniPattern");
+        inductive.add(new PatternPart("Name", "What shall this pattern be called by practitioners?"));
+        inductive.add(new PatternPart("Template", "What shall this pattern be called by practitioners?"));
+        inductive.add(new PatternPart("Context", " What are the assumed environment or a priori assumptions for applying this pattern?"));
+        inductive.add(new PatternPart("Forces", "What are the different design motivations that must be balanced? ?"));
+        inductive.add(new PatternPart("Solution", "How do we solve the problem? "));
+
 
         // TODO: Add other templates
         templatesList.put("MicroPattern", micro);
+        templatesList.put("InductiveMiniPattern", inductive);
 
     }
 
