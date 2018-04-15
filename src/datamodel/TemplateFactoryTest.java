a84a5c827cd22babe426caf235a677e41a9d841b
Christos Zonios <czonios@gmail.com>
2018-04-15 21:30:25 +0300

    ADD tests for TemplateFactory class

--- src/datamodel/TemplateFactoryTest.java
+++ src/datamodel/TemplateFactoryTest.java
@@ -2,18 +2,41 @@ package datamodel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TemplateFactoryTest
-{
-//    @org.junit.jupiter.api.Test
-//    void shouldMakeTemplates()
-//    {
-//        String[] ex_result = {"default0", "default1", "default2"};
-//        TemplateFactory factory = new TemplateFactory();
-//        Pattern[] result = factory.templatesListIterator();
-//        for (int i = 0; i < 3; i++)
-//        {
-//            assertEquals(ex_result[i], result[i].name);
-//        }
-//    }
+class TemplateFactoryTest {
+
+    @org.junit.jupiter.api.Test
+    void shouldMakeTemplates() {
+
+        TemplateFactory tf = new TemplateFactory();
+        assertNotEquals(tf.getTemplatesList().isEmpty(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCloneDeepCopy() {
+
+        TemplateFactory tf = new TemplateFactory();
+        PatternComponent newPattern = tf.createTemplate("Micro-Pattern");
+        PatternComponent oldPattern = tf.getTemplatesList().get("Micro-Pattern");
+
+        newPattern.setName("asdf");
+        oldPattern.setName("qwer");
+
+        assertNotEquals(oldPattern.getName(), newPattern.getName());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCloneDeepCopyOfParts() {
+        TemplateFactory tf = new TemplateFactory();
+        Pattern newPattern = (Pattern) tf.createTemplate("Micro-Pattern");
+
+        PatternPart newPart = (PatternPart) newPattern.getComponentsList().get(0);
+        PatternPart oldPart = (PatternPart) tf.getTemplatesList().get("Micro-Pattern").getComponentsList().get(0);
+
+        newPart.setName("Lorem ipsum");
+        oldPart.setName("dolor sit amet");
+
+        assertNotEquals(newPart.getName(), oldPart.getName());
+    }
 
 }
