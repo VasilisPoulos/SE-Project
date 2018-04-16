86a096bbb3bd2e0bbdf94ec6629e470ca016f4e9
Vasilis Poulos <cse42805@cs.uoi.gr>
2018-04-16 21:04:42 +0300

    ADD test for deep copy.
    Using System.out's because i know it will fail at this point.

--- src/datamodel/PatternTest.java
+++ src/datamodel/PatternTest.java
@@ -2,6 +2,8 @@ package datamodel;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PatternTest {
@@ -23,16 +25,38 @@ class PatternTest {
 
     /**
      * Check if clone makes a deep copy
-     *      Make sure that cloning makes a new object.
-     *      If original components name changes, copy's name should stay
-     *      the same
+     * Make sure that cloning makes a new object.
+     * If original components name changes, copy's name should stay
+     * the same
      */
     @org.junit.jupiter.api.Test
-    void shouldClone()
-    {
+    void shouldClone() {
+        ArrayList<PatternComponent> newPatternList;
+        ArrayList<PatternComponent> cloneList;
+
         Pattern newPattern = new Pattern("newPattern");
         Pattern clone = newPattern.clone();
-        assertNotSame(newPattern,clone); //not the Same Object
+        assertNotSame(newPattern, clone); //not the Same Object
 
+        newPatternList = newPattern.getComponentsList();
+        PatternComponent comp1 = new PatternComponent("comp1");
+        newPattern.add(comp1);
+        PatternComponent comp2 = new PatternComponent("comp2");
+        newPattern.add(comp2);
+        for (PatternComponent p : newPatternList) {
+            System.out.println(p.getName());
+        }
+
+        PatternComponent comp3 = new PatternComponent("comp3");
+        clone.add(comp3);
+        cloneList = clone.getComponentsList();
+        for (PatternComponent p : cloneList) {
+            System.out.println(p.getName());
+        }
+
+        System.out.println("Last iteration");
+        for (PatternComponent p : newPatternList) {
+            System.out.println(p.getName());
+        }
     }
 }
