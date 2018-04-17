200a1c95e54db7bb0a1cc8a5f843749262d7409b
Vasilis Poulos <cse42805@cs.uoi.gr>
2018-04-17 23:37:48 +0300

    REMOVED empy test class , more code gardening

--- src/datamodel/PatternCompositeTest.java
+++ src/datamodel/PatternCompositeTest.java
@@ -1,7 +1,5 @@
 package datamodel;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatternCompositeTest {
--- src/datamodel/PatternTest.java
+++ src/datamodel/PatternTest.java
@@ -8,21 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PatternTest{
 
-    /*
-    @org.junit.jupiter.api.Test
-    void shouldClonePattern()
-    {
-        PatternComponent component = new PatternComponent("Pattern");
-        PatternComponent copy = component.clone();
-        assertNotSame(component,copy); //not the Same Object
-        assertEquals("Pattern",component.getName());
-        assertEquals("Pattern",copy.getName());
-        component.setName("New name");
-        assertEquals("New name",component.getName());
-        assertEquals("Pattern",copy.getName());
-    }
-    */
-
     /**
      * Check if clone makes a deep copy
      * Make sure that cloning makes a new object.
